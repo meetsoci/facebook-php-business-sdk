44981b7d52fe936e04b131e4ffa61f87390142ca
Christian <christian@meetsoci.com>
2026-04-14 09:07:50 -0700

    Fixing for php 7.2

--- src/FacebookAds/Object/ServerSide/AttributionSetting.php
+++ src/FacebookAds/Object/ServerSide/AttributionSetting.php
@@ -141,7 +141,7 @@ class AttributionSetting implements ArrayAccess {
    * @param integer $offset Offset
    * @return mixed
    */
-  public function offsetGet($offset) : mixed {
+  public function offsetGet($offset) {
     return isset($this->container[$offset]) ? $this->container[$offset] : null;
   }
 
